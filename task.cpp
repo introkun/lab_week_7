b54187e4bd26edc6ebd71d56b5c516371e7c3920
setlol <dovnar.maxim@gmail.com>
2016-07-08 16:02:16 +0300

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,42 @@
+﻿#include <locale.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int star, center;
+
+	cout << "выводим ромб, размер не четный\nвведите размер = ";
+	cin >> star;
+	if (star%2 == 0)
+	{
+		cout << "вы ввели неверные данные!!!\n";
+		return 1;
+	}
+	center = star / 2;
+
+	for (int i = 0; i < star; i++)
+	{
+		for (int j = 0; j < star; j++)
+		{
+			if (i <= center)
+			{
+				if (j >= center - i && j <= center + i)
+					cout << "*";
+				else
+					cout << " ";
+			}
+			else
+			{
+				if (j >= center + i - star + 1 && j <= center - i + star - 1)
+					cout << "*";
+				else
+					cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ task1.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int total, intotal, cycle = 0, ok = 0, evaluation, attempts = 1, max1, max2, i1, i2;
+	int complexity;
+	srand(time(NULL));
+	cout << "проверяем на знание таблицы умножения" "\n"
+		"Программа выводит на экран два числа, пользователь должен ввести их произведение" "\n"
+		"выбирете уровней сложности" "\n"
+		"Easy = 1 " "\n"
+		"Normal = 2" "\n"
+		"Medium = 3" "\n"
+		"Hard = 4" "\n"
+		"Master = 5" "\n"
+		"выход = 0 " "\n" "сделайте выбор: ";
+	cin >> complexity;
+	do
+	{
+		if (complexity == 0)
+		{
+			break;
+		}
+		switch (complexity)
+		{
+		case 1:
+			attempts = 5;
+			max1 = 10;
+			max2 = 10;
+			i1 = 1;
+			i2 = 1;
+			break;
+		case 2:
+			attempts = 10;
+			max1 = 100;
+			max2 = 10;
+			i1 = 10;
+			i2 = 1;
+			break;
+		case 3:
+			attempts = 15;
+			max1 = 100;
+			max2 = 100;
+			i1 = 10;
+			i2 = 10;
+			break;
+		case 4:
+			attempts = 25;
+			max1 = 1000;
+			max2 = 100;
+			i1 = 100;
+			i2 = 10;
+			break;
+		case 5:
+			attempts = 50;
+			max1 = 1000;
+			max2 = 1000;
+			i1 = 100;
+			i2 = 100;
+		default: 
+		break;
+		}
+		int number1 = rand() % max1 + i1;
+		int number2 = rand() % max2 + i2;
+		cout << number1 << " * ";
+		cout << number2 << " = ";
+		cin >> intotal;
+		total = number1 * number2;
+		if (total == intotal)
+		{
+			cout << "верно" << endl;
+			ok++;
+		}
+		else
+		{
+			cout << "верный ответ = " << total << endl;
+		}
+		cycle++;
+	} while (cycle < attempts);
+	evaluation = 10 * ok / attempts;
+	cout << "итог: " "\n" "правельных ответов " << ok << " из " << attempts << "\n"
+		"ваша оценка " << evaluation << " из 10" "\n";
+}
+
+
+/*Написать программу, которая проверяет пользователя на знание таблицы умножения.
+Программа выводит на экран два числа, пользователь должен ввести их произведение.
+Разработать несколько уровней сложности (отличаются сложностью и количеством вопросов).
+Вывести пользователю оценку его знаний.*/
